d2bf06387c70817ea2993cc38c15de6d482b3d22
C08M <bladimyr917@gmail.com>
2026-01-12 20:53:16 -0500

    logica de la calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        /*RAMA DE COLABORADOR 3 - Cesar*/
+        Scanner teclado = new Scanner(System.in);
 
+        switch (opcion){
+            case 1:
+                int suma=num1+num2;
+                System.out.println("El resultado de la suma es" + suma);
+                break;
+            case 2:
+                int resta=num1+num2;
+                System.out.println("El resultado de la resta es" + resta);
+                break;
+            case 1:
+                int multiplicacion=num1+num2;
+                System.out.println("El resultado de la multiplicación es" + multiplicacion);
+                break;
+            case 4:
+
+                if (num2!=0) {
+                    int division=num1/num2;
+                    System.out.println("El resultado de la división es:" + division);
+                }else{
+                    System.out.println("Error no se puede dividir para 0");
+
+                }
+                break;
+            default:
+                System.out.println("Opción no valida :(");
+
+        }
+         teclado.close();
+
+    }
 }
