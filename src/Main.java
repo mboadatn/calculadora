1e2afe836f000e8c28a2c8cc21ba5e8cdcbc2951
veritozy <verozy26@gmail.com>
2026-01-12 20:53:13 -0500

    petición de datos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
+        /*RAMA COLABORADOR 1 - VERÓNICA*/
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Bienvenidos al Sistema :)");
+        System.out.println("Ingrese el primer número");
+        int num1 = teclado.nextInt();
 
+        System.out.println("Ingrese el primer número");
+        int num2 = teclado.nextInt();
+
+        }
 }
