4df46027fd8bfe0b0ea56f13d73b89e854a499fa
mboadatn2703@gmail.com <isaac270307@gmail.com>
2026-01-12 20:53:17 -0500

    Sumas basicas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
-        }
+    /*RAMA 2*/ {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Elija una opcion");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+
+
+        int opcion = teclado.nextInt();
+
     }
 }
